238be35c92e8ef6661436ce8ac009b56bff1faf6
tilak318 <155865818+tilak318@users.noreply.github.com>
2025-08-24 13:36:48 +0530

    Your commit message here

--- src/pages/BlogDetail.tsx
+++ src/pages/BlogDetail.tsx
@@ -47,7 +47,7 @@ const BlogDetail: React.FC = () => {
                 {blog.title}
               </h1>
               <p className="text-lg sm:text-xl md:text-2xl text-gray-700 font-medium">
-                By {blog.author}
+                Author: {blog.author}
               </p>
             </div>
           </div>
