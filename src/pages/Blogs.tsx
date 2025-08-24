e94a39f02f8344ecde5504301dcc62291335b27a
tilak318 <155865818+tilak318@users.noreply.github.com>
2025-08-24 12:07:16 +0530

    Your commit message here

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -96,8 +96,8 @@ const Blogs: React.FC = () => {
         </div>
         
         <div className="container mx-auto px-4 sm:px-6 lg:px-10 relative z-10 h-full">
-          <div className="flex flex-col justify-end h-full text-right pb-20">
-            <div className="max-w-2xl lg:max-w-xl xl:max-w-2xl ml-auto mr-16 sm:mr-24 lg:mr-32">
+          <div className="flex flex-col justify-end h-full text-center pb-20">
+            <div className="max-w-2xl lg:max-w-xl xl:max-w-2xl mx-auto">
               <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-6 sm:mb-8 leading-tight">
                 <span className="text-white drop-shadow-2xl font-extrabold">
                   Insurance
